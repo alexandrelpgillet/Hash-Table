84e11af5fda5f32b065682b69746445ad45e41e8
Alexandre Laureano <alexandrelgillet@gmail.com>
2025-10-20 18:34:53 -0300

    remove venv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np;
+from matplotlib import rcParams
 
 
 #Numero de colisões 
@@ -18,18 +19,59 @@ quant_string = ("100","1000","10000")
 
 colisionByM = {
     
-    '31' : (18.35,18.43,14.98),
-    '79' : (38.79, 48.83,47.50),
-    '151' : (189.95, 195.82, 217.19)      
+    '31' : (3.07,32.25,322.58),
+    '79' : (0.882, 12.65,126.58),
+    '151' : (0.319, 6.61, 66.22)      
 }
 
 
+#100;31;0.017970;3.070968
+#100;79;0.009550;0.882278
+#100;151;0.009260;0.319205
+#1000;31;0.010300;32.258064
+#1000;79;0.009590;12.658228
+#1000;151;0.010080;6.618543
+#10000;31;0.034970;322.580658
+#10000;79;0.020760;126.582275
+#10000;151;0.016680;66.225166
+#100000;31;0.335540;3225.806396
+#00000;79;0.152230;1265.822754
+#100000;151;0.092490;662.251648
+
+
+
+
+# plotting parameters
+rcParams.update({
+    'figure.figsize': (10, 6),
+    'font.size': 12,
+})
 
 x = np.arange(len(quant_string))
-width = 0.25
+num_bars = len(colisionByM)
+width = 0.8 / num_bars  # auto width so all bars fit nicely
+
+fig, ax = plt.subplots(layout='constrained')
+
+colors = plt.get_cmap('tab10').colors
 multiplier = 0
 
-fig, ax = plt.subplots(layout = 'constrained')
+for i, (attribute, measurement) in enumerate(colisionByM.items()):
+    offset = (i - (num_bars - 1) / 2) * width
+    rects = ax.bar(x + offset, measurement, width, label=f"M={attribute}", color=colors[i % len(colors)], edgecolor='black')
+    ax.bar_label(rects, padding=3, fmt='%.2f')
 
+ax.set_ylabel("Número de colisões (média)")
+ax.set_title("Colisões média por lista da hash table")
+# set xticks at the center of groups
+ax.set_xticks(x)
+ax.set_xticklabels(quant_string)
+ax.set_xlabel("Quantidade de strings (N)")
 
-for attribute, measurement in 
+ax.legend(loc='upper left', ncols=3, frameon=True)
+ax.set_ylim(0, max(max(v) for v in colisionByM.values()) * 1.15)
+ax.yaxis.grid(True, linestyle='--', alpha=0.6)
+
+plt.tight_layout()
+plt.show()
+ 
